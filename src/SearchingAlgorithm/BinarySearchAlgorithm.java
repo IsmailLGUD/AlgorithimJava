84ba52052a3fdf382b5f383a3fdc55f8c8f7877f
Ismail <mdismailhossain215@gmail.com>
2022-09-23 14:35:09 +0600

    Binary Search

--- src/SearchingAlgorithm/BinarySearchAlgorithm.java
+++ src/SearchingAlgorithm/BinarySearchAlgorithm.java
@@ -4,25 +4,25 @@ public class BinarySearchAlgorithm {
 
 	
 		  public static void main(String[] args) {
-		    int[] a = {1,2,3,4,5,6,7,8,9,12,13,14};
-		    int item = 4;
-		    
-		    int left = 0, right = a.length-1, middle;
+			  int[] a = {1,2,3,4,5,6,7,8,9,12,13,14};
+			    int item = 4;
+			    
+			    int left = 0, right = a.length-1;
 
-		    middle = (left+right)/2;
-		    while(left<=right){
-		    if(a[middle] == item){
-		      System.out.println("Item found at index: " + middle);
-		        return;
-		    }else if(a[middle]<item){
-		      left = middle +1;
-		    }else{
-		      right = middle -1;
-		    }
-		    
-		  }
+			    
+			    while(left<=right){
+			    int middle = left + (right-left)/2;
+			    if(a[middle] == item){
+			      System.out.println("Item found at index: " + middle);
+			        return;
+			    }else if(a[middle]<item){
+			      left = middle +1;
+			    }else{
+			      right = middle -1;
+			    }
+			    }
 
-		    System.out.println("System not found!!!!");
+			    System.out.println("System not found!!!!");
 		  
 		}
 
